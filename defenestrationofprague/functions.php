2f6c6976ad7f3835888e69cd45dd13a5451a82bc
Timtim <timkrow@centurylink.net>
2020-01-29 19:48:09 -0500

    Update functions.php

--- defenestrationofprague/functions.php
+++ defenestrationofprague/functions.php
@@ -30,6 +30,9 @@ if ( ! function_exists( 'defenestrationofprague_setup' ) ) :
 		// Add post formats
 		add_theme_support( 'post-formats',  array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
 
+		// Allow custom headers
+		add_theme_support( 'custom-header' );
+
 		/*
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
@@ -118,6 +121,36 @@ function defenestrationofprague_widgets_init() {
 	) );
 }
 add_action( 'widgets_init', 'defenestrationofprague_widgets_init' );
+
+
+/**
+ * Function to add support for custom headers
+ */
+function defenestrationofprague_custom_header_setup(){
+	$defaults = array(
+        // Default Header Image to display
+        'default-image'         => get_template_directory_uri() . '/images/headers/default.jpg',
+        // Display the header text along with the image
+        'header-text'           => false,
+        // Header text color default
+        'default-text-color'        => '000',
+        // Header image width (in pixels)
+        'width'             => 1000,
+        // Header image height (in pixels)
+        'height'            => 198,
+        // Header image random rotation default
+        'random-default'        => false,
+        // Enable upload of image file in admin 
+        'uploads'       => false,
+        // function to be called in theme head section
+        'wp-head-callback'      => 'wphead_cb',
+        //  function to be called in preview page head section
+        'admin-head-callback'       => 'adminhead_cb',
+        // function to produce preview markup in the admin screen
+        'admin-preview-callback'    => 'adminpreview_cb',
+        );
+}
+add_action('after_setup_theme', 'themename_custom_header_setup');
 
 /**
  * Enqueue scripts and styles.
