6f35f1fc04eaa8959c9909f7b0ddb448de1c3ab7
Timtim <timkrow@centurylink.net>
2020-01-31 09:03:13 -0500

    Update functions.php

--- defenestrationofprague/functions.php
+++ defenestrationofprague/functions.php
@@ -110,35 +110,6 @@ function defenestrationofprague_content_width() {
 add_action( 'after_setup_theme', 'defenestrationofprague_content_width', 0 );
 
 /**
- * Custom header to wordpress theme
- */
-function defenestrationofprague_custom_header_setup() {
-    $defaults = array(
-        // Default Header Image to display
-        'default-image'         => get_template_directory_uri() . '/images/headers/default.jpg',
-        // Display the header text along with the image
-        'header-text'           => false,
-        // Header text color default
-        'default-text-color'        => '000',
-        // Header image width (in pixels)
-        'width'             => 1000,
-        // Header image height (in pixels)
-        'height'            => 198,
-        // Header image random rotation default
-        'random-default'        => false,
-        // Enable upload of image file in admin 
-        'uploads'       => false,
-        // function to be called in theme head section
-        'wp-head-callback'      => 'wphead_cb',
-        //  function to be called in preview page head section
-        'admin-head-callback'       => 'adminhead_cb',
-        // function to produce preview markup in the admin screen
-        'admin-preview-callback'    => 'adminpreview_cb',
-        );
-}
-add_action( 'after_setup_theme', 'themename_custom_header_setup' );
-
-/**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
